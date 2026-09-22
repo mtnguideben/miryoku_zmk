4b6ba25f7de20ae9a6f2803de2787b91d1ad3a56
Ben <ben@benwood.work>
2026-09-22 14:53:44 -0400

    Update custom_config.h

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -5,3 +5,8 @@
 #define MIRYOKU_KLUDGE_SOFT_OFF
 #define MIRYOKU_KLUDGE_DOUBLETAPBOOT
 #define MIRYOKU_KLUDGE_GLOBALSHIFTFUNCTIONS
+#define MIRYOKU_LAYER_EXTRA \
+&kp Q,     &kp W,     &kp E,     &kp R,     &kp T,          &kp Y,     &kp U,     &kp I,      &kp O,     &kp P,               \
+&kp A,     &kp S,     &kp D,     &kp F,     &kp G,          &kp H,     &kp J,     &kp K,      &kp L,     &kp SQT,             \
+&kp Z,     &kp X,     &kp C,     &kp V,     &kp B,          &kp N,     &kp M,     &kp COMMA,  &kp DOT,   &kp FSLH,            \
+U_NP,      U_NP,      &kp LSHFT, &kp SPACE, &kp LCTRL,      &kp ESC,   &kp RET,   &u_to_U_BASE, U_NP,    U_NP
